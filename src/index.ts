c0b7e630446043fb2be0c60e83c4936b19e5e8c0
褚墨 <chumo.zll@alibaba-inc.com>
2025-06-27 17:17:48 +0800

    feat:修改服务内容

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
-#!/usr/bin/env node
-import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 
 const initialMcpServer = () => {
     const server = new McpServer({
         name: 'city-mcp',
-        version: '1.0.0',
+        version: '0.0.2',
         capabilities: {
             resources: {},
             tools: {},
         },
         instructions:
-            '该服务主要用于帮助用户查询城市热门景区'
+            "该服务主要用于在用户基于文本生成图片时，可以结合淘宝人生的人物图像做一个图像合并，可以帮用户生成带有淘宝人生人物形象的图片，请仔细理解用户的意图，并按以下指引选择合适的接口:\n\n" +
+            "**原则:**\n" +
+            "* **优先理解意图**: 判断用户的真实需求，是不是含有淘宝人生人物信息. \n" +
+            "* **清晰呈现结果**: 将接口返回的信息以用户能够易于理解的方式呈现. \n\n" +
+            "请根据上述指引选择接口",
     });
     return server;
 }
 
-const registerTool = (toolName: string, description: string, params: any) => {
+const registerTool = (toolName: string, description: string) => {
     const server = initialMcpServer();
     server.tool(
         toolName,
@@ -26,10 +29,10 @@ const registerTool = (toolName: string, description: string, params: any) => {
         },
         async () => {
             return {
-                content: [{ type: 'text', text: 'hello world' }]
+                content: [{ type: 'text', text: 'https://img.alicdn.com/imgextra/i1/O1CN01IfwUJW1gTXxcYIjSE_!!4611686018427385087-0-tblifeugc.jpg' }]
             }
         }
     )
 }
 
-registerTool('mcp-get-city-house', '获取城市房屋信息', {});
+registerTool('generateTBLifeImageWithBackground', '获取淘宝人生的人物形象，并把形象图和文生图进行图片融合生成一张图');
